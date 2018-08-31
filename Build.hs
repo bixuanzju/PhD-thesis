916d9cd57a8cfb0142039fc8731b756fda6cd6b7
Jeremy Bi <bixuanxbi@gmail.com>
2018-08-31 12:23:51 +0800

    refinement on build rules, again

--- Build.hs
+++ Build.hs
@@ -4,35 +4,35 @@ import Development.Shake.FilePath
 import Development.Shake.Util
 
 
-ottFile = "ott-rules.tex"
+ottFile = "Gen/ott-rules.tex"
 doc = "Thesis"
 ottFlags = "-tex_wrap false -tex_show_meta false"
 
 
 main :: IO ()
-main = shakeArgs shakeOptions{shakeFiles="_build"} $ do
-
+main =
+  shakeArgs shakeOptions {shakeFiles = "_build"} $ do
     want [doc <.> "pdf"]
 
     "*.pdf" %> \_ -> do
-        texSource <- getDirectoryFiles "" ["//*.tex"]
-        mngSource <- getDirectoryFiles "" ["//*.mngtex"]
-        let mngFiles = [c -<.> "mng" | c <- mngSource]
-        need $ [ottFile] ++ mngFiles ++ texSource
-        cmd "latexmk" [doc -<.> "tex"]
+      texSource <- getDirectoryFiles "" ["/*.tex", "Sources//*.tex"]
+      mngSource <- getDirectoryFiles "" ["Sources//*.mngtex"]
+      let genFiles = ["Gen" </> (dropDirectory1 c -<.> "tex") | c <- mngSource]
+      need $ [ottFile] ++ genFiles ++ texSource
+      cmd "latexmk" [doc -<.> "tex"]
 
-    "//*.mng" %> \out -> do
+    "Gen//*.tex" %> \out -> do
       ottSource <- getDirectoryFiles "" ["spec/*.ott"]
-      let dep = out -<.> ".mngtex"
+      let dep = "Sources" </> dropDirectory1 out -<.> ".mngtex"
       need $ dep : ottSource
       cmd "ott" ottSource ottFlags "-tex_filter" [dep] [out]
 
     ottFile %> \out -> do
-        ottSource <- getDirectoryFiles "" ["spec/*.ott"]
-        need ottSource
-        cmd "ott" ottSource ottFlags "-o" [out]
+      ottSource <- getDirectoryFiles "" ["spec/*.ott"]
+      need ottSource
+      cmd "ott" ottSource ottFlags "-o" [out]
 
     phony "clean" $ do
-        putNormal "Cleaning files in _build"
-        removeFilesAfter "_build" ["//*"]
-        cmd "latexmk -C"
+      putNormal "Cleaning files in _build"
+      removeFilesAfter "_build" ["//*"]
+      cmd "latexmk -c"
